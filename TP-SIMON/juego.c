507cfcc6170847bae297ccb771c29cdf433ac498
jessidirosa <jessica.dirosa@yahoo.com.ar>
2025-09-28 17:27:38 -0300

    funciones: partida de juego y actualizar archivo rankings

--- TP-SIMON/juego.c
+++ TP-SIMON/juego.c
@@ -1,6 +1,49 @@
 #include "juego.h"
 
-bool iniciarJuego()
+void iniciarJuego(tPartida* juego) //comienza la partida
 {
+    //todo esto podria estar una funcion iniciarPartida
+    //el nombre del jugador ya llega en la struct
+    juego->psec = juego->sec;
+    juego->ranking->score = 0;
+    juego->estado = CONTINUA;
+    juego->tiempoNota = TIEMPO_INICIAL; //puede que ya llegue a la funcion con esto ya asignado porque se cambia en la configuracion
+    juego->acoteDuracion = TIEMPO_ACOTADO_POR_NOTA;
 
+    while(juego->estado != GAMEOVER)
+    {
+        secuencia(juego); //mostrara la secuencia desde sec a psec a medida que avanza
+
+        if(respuesta(juego) == ERROR) //registrara la respuesta del jugador y la ira guardando en res hasta detectar un error en el patron
+            juego->estado = GAMEOVER;
+
+        else
+        {
+            juego->ranking->score++;
+            juego->psec++;
+            disminuirTiempo(juego);
+        }
+    }
+}
+
+//hacer funcion de crearArchivoRanking y ordenarArchivo
+bool insertarArchivoRankingSinDup(tPartida* juego)
+{
+    FILE* pf = fopen("Rankings.bin", "r+b");
+    tRanking rankArch;
+
+    if(!pf)
+        return false; //no se pudo abrir
+
+    fread(&rankArch, sizeof(tRanking), 1, pf);
+    while(!feof(pf) && strcmp(juego->ranking->jugador,rankArch.jugador))
+        fread(&rankArch, sizeof(tRanking), 1, pf);
+
+    if(!strcmp(juego->ranking->jugador,rankArch.jugador))
+        fseek(pf, -1L, SEEK_CUR);
+
+    fwrite(juego->ranking, sizeof(tRanking), 1, pf);
+    fclose(pf);
+
+    return true; //todo ok
 }
--- TP-SIMON/juego.h
+++ TP-SIMON/juego.h
@@ -4,8 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX 1024
+#define MAX_NOMBRE 20
+#define GAMEOVER 0
+#define ERROR -1
+#define CONTINUA 1
+#define TIEMPO_INICIAL 2000
+#define TIEMPO_ACOTADO_POR_NOTA 0.03
 
 typedef struct {
     int rojo; //1
@@ -21,12 +28,22 @@ typedef struct {
 
 typedef struct
 {
-    int secuencia[MAX];
+    char jugador[MAX_NOMBRE];
+    int score;
+} tRanking;
+
+typedef struct
+{
+    int sec[MAX];
     int* psec;
-    int respuesta;
+    int res[MAX];
+    tRanking* ranking;
     bool estado;
+    float tiempoNota;
+    float acoteDuracion;
 
 } tPartida;
+
 
 
 #endif // JUEGO_H_INCLUDED
